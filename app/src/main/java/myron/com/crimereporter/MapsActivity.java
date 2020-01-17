e1017ce3f88b7ca687080c211180911eae367a07
Myron Fernandes <1710183@brunel.ac.uk>
2020-01-17 23:24:30 +0000

    Added geocordinates

--- app/src/main/java/myron/com/crimereporter/MapsActivity.java
+++ app/src/main/java/myron/com/crimereporter/MapsActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -18,6 +20,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -29,12 +32,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toast.makeText(this,"Map is ready",Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
         nMap = googleMap;
+
+        if (mLocationPermissionGranted){
+            getDeviceLocation();
+
+            nMap.setMyLocationEnabled(true);
+            nMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
     }
 
     private static final String TAG = "MapsActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
     //Variables
     private Boolean mLocationPermissionGranted = false;
     private GoogleMap nMap;
@@ -62,9 +73,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     public void onComplete(@NonNull Task task) {
                         if (task.isSuccessful()){
                           Log.d(TAG, "onComplete: Found Location");
-                          Location urrentLocation = (Location) task.getResult();
+                          Location currentLocation = (Location) task.getResult();
 
-                          
+                          moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
                         }else{
                             Log.d(TAG, "onComplete: Current Location is NULL");
                             Toast.makeText(MapsActivity.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
@@ -77,6 +89,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(TAG, "getDeviceLocation: securityexception: " + e.getMessage());
 
         }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: moving camera to: latitude: " + latLng.latitude + ",longitude: " + latLng.longitude);
+        nMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     private void initialiseMap(){
