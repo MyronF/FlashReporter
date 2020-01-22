87cf53903629a435fb12f990c37e9088d1ec2f40
Myron Fernandes <1710183@brunel.ac.uk>
2020-01-22 20:54:20 +0000

    minor tweaks

--- app/src/main/java/myron/com/crimereporter/MapsActivity.java
+++ app/src/main/java/myron/com/crimereporter/MapsActivity.java
@@ -1,7 +1,11 @@
 package myron.com.crimereporter;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -26,7 +30,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -34,6 +41,7 @@ import java.io.IOException;
 import java.security.KeyException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -42,19 +50,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this,"Map is ready",Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: map is ready");
-        nMap = googleMap;
+        mMap = googleMap;
 
         if (mLocationPermissionGranted){
             getDeviceLocation();
 
-            nMap.setMyLocationEnabled(true);
-            nMap.getUiSettings().setMyLocationButtonEnabled(false);
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
             init();
+
         }
     }
 
     private static final String TAG = "MapsActivity";
+
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQEST_CODE = 1234;
@@ -66,7 +76,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     //Variables
     private Boolean mLocationPermissionGranted = false;
-    private GoogleMap nMap;
+    private GoogleMap mMap;
     private FusedLocationProviderClient mfusedLocationProviderclient;
 
     @Override
@@ -77,10 +87,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         nSearchText = (EditText) findViewById(R.id.input_search);
 
         getLocationPermission();
-
-        init();
     }
 
+    //overide the enter key on the map
     private void init(){
         Log.d(TAG, "init: initialising");
 
@@ -103,6 +112,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Log.d(TAG, "geoLocate: geolocating");
 
         String searchString = nSearchText.getText().toString();
+
         Geocoder geocoder = new Geocoder(MapsActivity.this);
         List<Address> list = new ArrayList<>();
 
@@ -119,9 +129,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(TAG, "geoLocate: Found a location: " + address.toString());
 //            Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
 
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
+
         }
-
-
     }
 
     private void getDeviceLocation(){
@@ -132,7 +142,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         try{
             if (mLocationPermissionGranted){
 
-                Task location = mfusedLocationProviderclient.getLastLocation();
+                final Task location = mfusedLocationProviderclient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(@NonNull Task task) {
@@ -140,7 +150,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                           Log.d(TAG, "onComplete: Found Location");
                           Location currentLocation = (Location) task.getResult();
 
-                          moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                          moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM, "My location");
 
                         }else{
                             Log.d(TAG, "onComplete: Current Location is NULL");
@@ -154,11 +164,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(TAG, "getDeviceLocation: securityexception: " + e.getMessage());
 
         }
+
     }
 
-    private void moveCamera(LatLng latLng, float zoom){
+    private void moveCamera(LatLng latLng, float zoom, String title){
         Log.d(TAG, "moveCamera: moving camera to: latitude: " + latLng.latitude + ",longitude: " + latLng.longitude);
-        nMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+        mMap.addMarker(options);
     }
 
     private void initialiseMap(){
